0f6516f962fb61eb61f6b6cdbe1b7902d748251d
khbraham <brkhaider2001@gmail.com>
2024-11-06 19:34:54 +0100

    i want to host my website

---
+++ script.js
@@ -0,0 +1,77 @@
+
+function show_side_bar() {
+    const element = document.getElementById("side-bar");
+    element.classList.toggle("hidden");       
+}
+
+// Function to toggle the dropdown
+function toggleNotifications() {
+    const dropdown = document.getElementById("notificationDropdown");
+    dropdown.classList.toggle("hidden");
+}
+
+function toggleAddProjects() {
+    const dropdown = document.getElementById("projectsAddPrompt");
+    dropdown.classList.toggle("hidden");
+}
+
+function annulerAddProject() {
+    const dropdown = document.getElementById("projectsAddPrompt");
+    dropdown.classList.add("hidden");
+}
+
+// Close dropdown if clicking outside of it
+document.addEventListener("click", function(event) {
+    const dropdown = document.getElementById("notificationDropdown");
+    const notificationButton = document.querySelector("#notifications");
+
+    // Check if the click was outside the dropdown and the button
+    if (!dropdown.contains(event.target) && !notificationButton.contains(event.target)) {
+        dropdown.classList.add("hidden");
+    }
+});
+
+// Close dropdown when Escape key is pressed
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Escape") {
+        const dropdown = document.getElementById("notificationDropdown");
+        if (!dropdown.classList.contains("hidden")) {
+            dropdown.classList.add("hidden");
+        }
+    }
+});
+
+function toggleProfil() {
+    const dropdown = document.getElementById("profilDropdown");
+    dropdown.classList.toggle("hidden");
+}
+
+document.addEventListener("click", function(event) {
+    const dropdown = document.getElementById("profilDropdown");
+    const notificationButton = document.querySelector("#profil");
+
+    // Check if the click was outside the dropdown and the button
+    if (!dropdown.contains(event.target) && !notificationButton.contains(event.target)) {
+        dropdown.classList.add("hidden");
+    }
+});
+
+// Close dropdown when Escape key is pressed
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Escape") {
+        const dropdown = document.getElementById("profilDropdown");
+        if (!dropdown.classList.contains("hidden")) {
+            dropdown.classList.add("hidden");
+        }
+    }
+});
+
+function toggleAddMembers() {
+    const dropdown = document.getElementById("membersAddPrompt");
+    dropdown.classList.toggle("hidden");
+}
+
+function annulerAddMembers() {
+    const dropdown = document.getElementById("membersAddPrompt");
+    dropdown.classList.add("hidden");
+}
